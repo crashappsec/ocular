ad4dd1c4be2452cd55b33b360658699751fe32b9
Bryce Thuilot <bryce@crashoverride.com>
2026-05-06 13:37:42 -0400

    fix(helm): correct whitespace for volumes and enviornment
    Signed-off-by: Bryce Thuilot <bryce@crashoverride.com>

--- hack/kubebuilder-helm-patch/patch.go
+++ hack/kubebuilder-helm-patch/patch.go
@@ -51,24 +51,26 @@ var replacements = map[string][]replacement{
 				"${1}  {{- end}}",
 		},
 		{
-			Pattern: regexp.MustCompile(`(?m)^([ ]+)env: (\[\])?`),
+			Pattern: regexp.MustCompile(`(?m)^([ ]+)env: \[\]`),
 			Replacement: "${1}env:\n" +
-				"${1}  {{- with .Values.manager.env }}\n" +
-				"${1}  {{- toYaml . | nindent 10 }}\n" +
+				"${1}  {{- if .Values.manager.env }}\n" +
+				"${1}  {{- toYaml .Values.manager.env | nindent 8 }}\n" +
+				"${1}  {{- else}}\n" +
+				"${1}  []\n" +
 				"${1}  {{- end}}",
 		},
 		{
 			Pattern: regexp.MustCompile(`(?m)^([ ]+)volumeMounts:`),
 			Replacement: "${1}volumeMounts:\n" +
-				"${1}  {{- with .Values.manager.volumeMounts }}\n" +
-				"${1}  {{- toYaml . | nindent 10}}\n" +
+				"${1}  {{- if .Values.manager.volumeMounts }}\n" +
+				"${1}  {{- toYaml .Values.manager.volumeMounts | nindent 8}}\n" +
 				"${1}  {{- end}}",
 		},
 		{
 			Pattern: regexp.MustCompile(`(?m)^([ ]+)volumes:`),
 			Replacement: "${1}volumes:\n" +
-				"${1}  {{- with .Values.manager.volumes }}\n" +
-				"${1}  {{- toYaml . | nindent 8}}\n" +
+				"${1}  {{- if .Values.manager.volumes }}\n" +
+				"${1}  {{- toYaml .Values.manager.volumes | nindent 6}}\n" +
 				"${1}  {{- end}}",
 		},
 	},
